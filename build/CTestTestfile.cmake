0518ec56780f3981f8677be72fcdce9bf082a5f9
Ivan <ivan.vujevic@fer.hr>
2016-05-02 23:22:23 +0200

    deleted build dir

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vujevic/TacLast/Tachyon
-# Build directory: /home/vujevic/TacLast/Tachyon/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(deps)
-subdirs(src)
--- build/deps/CMakeFiles/tac_deps.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libtac_deps.a"
-)
